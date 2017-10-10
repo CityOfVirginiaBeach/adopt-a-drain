60369bdf9a4009687f0e9a3e6b248f75b02d10c9
Jason M. Hall <JMHall@vbgov.com>
2017-10-10 15:51:37 -0400

    updated code to pull in vb data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,71 @@
-User.where(email: 'john@example.com').first_or_initialize.tap do |user|
-  user.first_name = 'John'
-  user.last_name = 'Doe'
-  user.password = 'password'
-  user.save!
-end
+require 'csv'
+require 'open-uri'
 
-User.where(email: 'admin@example.com').first_or_initialize.tap do |user|
-  user.first_name = 'Jane'
-  user.last_name = 'Doe'
-  user.password = 'password'
-  user.admin = true
-  user.save!
-end
 
-r = Random.new
 
-500.times do |i|
-  Thing.where(city_id: i).first_or_initialize.tap do |thing|
-    thing.name = "Some Drain #{i}"
-    thing.lat = r.rand(37.75..37.78)
-    thing.lng = r.rand(-122.43..-122.41)
-    thing.system_use_code = ['MS4', 'STORM', 'COMB', 'UNK'].sample
-    thing.save!
-  end
+# url = '/Users/eddie/Downloads/drainswgs84.csv'
+# url = 'http://data.openoakland.org/storage/f/2012-11-01T014902/Inlets.csv'
+url = 'https://dev.vbgov.com/_assets/_apis/CVB-Drains.csv'
+
+# puts 'removing old things data'
+# Thing.destroy_all
+create_thing = 0
+update_thing = 0          
+failed_update = 0
+nil_thing = 0
+puts 'connecting'
+open(url) do |f|
+  puts 'downloading'
+  f.each_line do |l|
+    CSV.parse(l) do |row|
+      
+      if(row[0] == "OBJECTID_1")
+        next
+      else
+        city_id = row[0].to_i
+        lat = row[6].to_f
+        lng = row[7].to_f
+        puts "#{row} "        
+
+        if city_id > 1        
+          
+          
+          
+          drain = Thing.find_by_city_id( city_id )
+          
+          puts "#{city_id} #{lng} #{lat}"
+          
+          if drain
+            puts "UPDATING #{city_id} #{lng} #{lat} "                    
+            update_thing = update_thing + 1            
+          else
+            puts "CREATING NEW #{city_id} #{lng} #{lat} "        
+            drain = Thing.new({:city_id => city_id, :lng => lng, :lat => lat})          
+            create_thing = create_thing + 1            
+          end
+          
+
+          updated_successul = false
+          if !lng.nil? && !lat.nil?
+            drain.update_attributes({:lng => lng, :lat => lat})          
+            updated_successul = drain.save!            
+          else
+            nil_thing = nil_thing + 1            
+            
+          end
+          
+          
+          
+          if updated_successul
+            puts "SAVED"                                             
+          else
+            puts "FAILED"
+            failed_update = failed_update + 1                        
+          end
+
+
+        end
+      end
+    end
+  end 
 end
--- lib/tasks/data.rake
+++ lib/tasks/data.rake
@@ -3,7 +3,6 @@ require 'rake'
 namespace :data do
   task load_things: :environment do
     require 'thing_importer'
-
     ThingImporter.load('https://dev.vbgov.com/_assets/_apis/CVB-Drains.csv')
   end
 end
